1eee7950c14d347aea60e3be526c152f557b4878
yongli-australia <yong.li@xrfscientific.com>
2016-04-22 11:35:26 +1000

    upload a header file-create with eclipse

---
+++ code/tfread.h
@@ -0,0 +1,21 @@
+/*
+ * tfread.h
+ *
+ *  Created on: 21/04/2016
+ *      Author: Yong li
+ */
+
+#ifndef TFREAD_H_
+#define TFREAD_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <string.h>
+
+
+
+const char Wordfile[] = "/usr/share/dict/word.list";
+
+#endif /* TFREAD_H_ */
